f0917b7f98265e388a4066f5d96814fc42c00394
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-07-09 11:49:39 +0900

    :memo: (website) Add svelte and react page

---
+++ website/svelte/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import { Utterances } from 'utterances-svelte-component'
+</script>
+
+<main>
+  <h1>utterances-svelte-component</h1>
+
+  <p>This page works svelte</p>
+
+  <div class="space-x-2">
+
+  <a href="/">Home</a>
+  <a href="/react">React</a>
+  <a href="/vue">Vue</a>
+  </div>
+
+  <Utterances repo="TomokiMiyauci/utterances-component" theme="github-light" issueTerm="pathname" label="comment" />
+</main>
+
+<style>
+main {
+  max-width: 760px;
+  margin: 0 auto;
+}
+.space-x-2 > :not([hidden]) ~ :not([hidden]) {
+  --tw-space-x-reverse: 0;
+  margin-right: calc(0.5rem * var(--tw-space-x-reverse));
+  margin-left: calc(0.5rem * calc(1 - var(--tw-space-x-reverse)));
+}
+
+</style>
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import preact from '@preact/preset-vite'
 import vue from '@vitejs/plugin-vue'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [preact(), vue()],
+  plugins: [preact(), vue(), svelte()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         react: resolve(__dirname, 'react/index.html'),
-        vue: resolve(__dirname, 'vue/index.html')
+        vue: resolve(__dirname, 'vue/index.html'),
+        svelte: resolve(__dirname, 'svelte/index.html')
       }
     }
   }
