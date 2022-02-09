691022f07f7979cbf5ef8306e4747638e3ba6522
sage <laymonage@gmail.com>
2022-02-09 14:11:54 +0700

    web: Rename UMD format to use cjs extension and bump to 0.0.7

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
     lib: {
       entry: 'src/giscus.ts',
       name: 'giscus',
+      fileName: (format) => {
+        if (format === 'umd') return 'giscus.umd.cjs';
+        return 'giscus.es.js';
+      },
     },
     rollupOptions: {
       external: /^lit/,
