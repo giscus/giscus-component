47cca8070052556898a161c70fd49e226873c0ec
Sage Abdullah <me@laymonage.com>
2022-03-27 22:24:31 +0700

    svelte: Use spaces and limit width to 80 in Prettier config

--- svelte/src/lib/Giscus.svelte
+++ svelte/src/lib/Giscus.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import type {
-		AvailableLanguage,
-		BooleanString,
-		InputPosition,
-		Loading,
-		Mapping,
-		Theme
-	} from '$lib';
+  import { onMount } from 'svelte';
+  import type {
+    AvailableLanguage,
+    BooleanString,
+    InputPosition,
+    Loading,
+    Mapping,
+    Theme,
+  } from '$lib';
 
-	onMount(() => import('giscus'));
+  onMount(() => import('giscus'));
 
-	export let id: string = undefined;
-	export let repo: `${string}/${string}`;
-	export let repoId: string;
-	export let category: string = undefined;
-	export let categoryId: string = undefined;
-	export let mapping: Mapping = 'pathname';
-	export let term: string = undefined;
-	export let reactionsEnabled: BooleanString = '1';
-	export let emitMetadata: BooleanString = '0';
-	export let inputPosition: InputPosition = 'bottom';
-	export let theme: Theme = 'light';
-	export let lang: AvailableLanguage = 'en';
-	export let loading: Loading = 'eager';
+  export let id: string = undefined;
+  export let repo: `${string}/${string}`;
+  export let repoId: string;
+  export let category: string = undefined;
+  export let categoryId: string = undefined;
+  export let mapping: Mapping = 'pathname';
+  export let term: string = undefined;
+  export let reactionsEnabled: BooleanString = '1';
+  export let emitMetadata: BooleanString = '0';
+  export let inputPosition: InputPosition = 'bottom';
+  export let theme: Theme = 'light';
+  export let lang: AvailableLanguage = 'en';
+  export let loading: Loading = 'eager';
 </script>
 
 <main>
-	<giscus-widget
-		{id}
-		{repo}
-		repoid={repoId}
-		{category}
-		categoryid={categoryId}
-		{mapping}
-		{term}
-		reactionsenabled={reactionsEnabled}
-		emitmetadata={emitMetadata}
-		inputposition={inputPosition}
-		{theme}
-		{lang}
-		{loading}
-	/>
+  <giscus-widget
+    {id}
+    {repo}
+    repoid={repoId}
+    {category}
+    categoryid={categoryId}
+    {mapping}
+    {term}
+    reactionsenabled={reactionsEnabled}
+    emitmetadata={emitMetadata}
+    inputposition={inputPosition}
+    {theme}
+    {lang}
+    {loading}
+  />
 </main>
