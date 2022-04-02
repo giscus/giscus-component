3b9f3812ff942bcae7867a18251a5041e53223b6
Sage Abdullah <me@laymonage.com>
2022-04-02 21:21:55 +0700

    vue: Fix type declaration emits

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import typescript from '@rollup/plugin-typescript';
 
 const resolvePath = (str: string) => resolve(__dirname, str);
 
@@ -21,14 +20,6 @@ export default defineConfig({
           vue: 'Vue',
         },
       },
-      plugins: [
-        typescript({
-          target: 'es2020',
-          rootDir: resolvePath('src/lib'),
-          declaration: true,
-          declarationDir: resolvePath('dist'),
-        }),
-      ],
     },
   },
 });
