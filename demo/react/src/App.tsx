9e5c72516e02d1ee2706eb3654302715f30dc24f
Sage Abdullah <me@laymonage.com>
2022-04-03 13:19:40 +0700

    demo: Init React demo

---
+++ demo/react/src/App.tsx
@@ -0,0 +1,21 @@
+import Giscus from '@giscus/react';
+
+function App() {
+  return (
+    <Giscus
+      repo="giscus/giscus-component"
+      repoId="MDEwOlJlcG9zaXRvcnkzOTEzMTMwMjA="
+      category="Announcements"
+      categoryId="DIC_kwDOF1L2fM4B-hVS"
+      mapping="specific"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="light"
+      lang="en"
+    />
+  );
+}
+
+export default App;
