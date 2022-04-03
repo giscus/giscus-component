8ce880f10d5c35e861317e78fb3be40552704936
Sage Abdullah <me@laymonage.com>
2022-04-03 14:12:15 +0700

    react: Externalize react-dom and react/jsx-runtime

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -15,10 +15,12 @@ export default defineConfig({
       fileName: (format) => `giscus.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'reactJsxRuntime',
         },
       },
       plugins: [
