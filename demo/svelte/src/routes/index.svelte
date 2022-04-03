e91d99067d2f4f29f907d4590c1a09f008b9e5ba
Sage Abdullah <me@laymonage.com>
2022-04-03 13:32:20 +0700

    demo: Init Svelte demo

---
+++ demo/svelte/src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Giscus } from '@giscus/svelte';
+</script>
+
+<main>
+  <Giscus
+    repo="giscus/giscus-component"
+    repoId="MDEwOlJlcG9zaXRvcnkzOTEzMTMwMjA="
+    category="Announcements"
+    categoryId="DIC_kwDOF1L2fM4B-hVS"
+    mapping="specific"
+    term="Welcome to @giscus/svelte component!"
+    reactionsEnabled="1"
+    emitMetadata="0"
+    inputPosition="top"
+    theme="light"
+    lang="en"
+  />
+</main>
