23c0af53599704efdb8daa3b33b4b8f9302c9394
Sage Abdullah <me@laymonage.com>
2023-07-22 15:45:23 +0100

    react: Replace ReactDOM.render() with createRoot().render()

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root') as HTMLDivElement);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
